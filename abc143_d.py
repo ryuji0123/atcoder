18b20d36be2e7ab7bb6f2442e064c60f717959ad
ryuji0123 <ryo1011.javelin@gmail.com>
2019-10-20 19:06:35 +0900

    add two pointers method

--- abc143_d.py
+++ abc143_d.py
@@ -16,8 +16,19 @@
 #    #print("Not found\nmp: ", mp)
 #    return mp if target < nums[mp] else -1
 
-from bisect import *
+#from bisect import *
+#if __name__ == '__main__':
+#    N = int(input())
+#    L = sorted(map(int, input().split()))
+#    print(sum(jdx - bisect(L, a - L[jdx], 0, jdx) for idx, a in enumerate(L) for jdx in range(idx)))
+
 if __name__ == '__main__':
     N = int(input())
     L = sorted(map(int, input().split()))
-    print(sum(jdx - bisect(L, a - L[jdx], 0, jdx) for idx, a in enumerate(L) for jdx in range(idx)))
+    ret = 0
+    for adx in range(N):
+        cdx = adx
+        for bdx in range(adx + 1, N):
+            while cdx < N and L[cdx] < L[bdx] + L[adx]: cdx += 1
+            ret += cdx - (bdx + 1)
+    print(ret)
