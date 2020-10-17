d5e4d3d03e69778e4299ec7cc53e6d52c80108fd
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-18 01:50:42 +0900

    add abc180_c

---
+++ abc180_c.py
@@ -0,0 +1,19 @@
+def make_divisors(n):
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n//i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+
+
+def solve(N):
+    for i in make_divisors(N):
+        print(i)
+
+
+N = int(input())
+solve(N)
