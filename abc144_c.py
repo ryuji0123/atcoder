8ef121fda2d2fc84a090e3712cec0d34f76a637d
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-02 13:58:34 +0900

    uodate abc144_c

--- abc144_c.py
+++ abc144_c.py
@@ -1,6 +1,15 @@
+# time: O(N^(1/2))
+# space: O(1)
+
+def solve():
+    i = 1
+    ret = float('inf')
+    while i * i <= N:
+        if N % i == 0:
+            ret = min(ret, i - 1 + N // i - 1)
+        i += 1
+    print(ret)
+
+
 N = int(input())
-ret = 1 - 1 + N - 1
-for div in range(2, int(N ** 0.5) + 1):
-    if N % div != 0: continue
-    ret = min(ret, div - 1 + N // div - 1)
-print(ret)
+solve()
