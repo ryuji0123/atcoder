c9ca899b331deb60aa8081ad0ceeae96b5da2503
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-05 08:38:16 +0900

    modify abc135_c.py

--- abc135_c.py
+++ abc135_c.py
@@ -1,21 +1,19 @@
-def calcRestNum(a, b, ret):
-    if a < b:
-        ret += a
-        b -= a
-        a = 0
-    else:
-        ret += b
-        a -= b
-        b = 0
-    return [a, b, ret]
-
-if __name__ == '__main__':
-    N = int(input())
-    A = [int(a) for a in input().split()]
-    B = [int(b) for b in input().split()]
-
+def solve():
     ret = 0
-    for n in reversed(range(N)):
-        A[n + 1], B[n], ret = calcRestNum(A[n + 1], B[n], ret)
-        A[n], B[n], ret = calcRestNum(A[n], B[n], ret)
+    for i in range(N - 1, -1, -1):
+        for j in range(i  +1, i - 1, -1):
+            if A[j] <= B[i]:
+                ret += A[j]
+                B[i] -= A[j]
+                A[j] = 0
+            else:
+                ret += B[i]
+                A[j] -= B[i]
+                B[i] = 0
     print(ret)
+
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+solve()
