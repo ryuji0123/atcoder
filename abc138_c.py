3be9b97994361ea0e012a6b7b759428aa295301e
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-04 20:49:14 +0900

    modify abc138_c.py

--- abc138_c.py
+++ abc138_c.py
@@ -1,10 +1,14 @@
+# time: O(N)
+# space: O(1)
 
-if __name__ == '__main__':
-    N = int(input())
-    values = sorted([int(n) for n in input().split()], reverse=True)
-    ret = 0
-    while len(values) > 1:
-        tmp = (values[-1] + values[-2]) / 2
-        del values[-1]
-        values[-1] = tmp
-    print(values[0])
+def solve():
+    ret = v[0]
+    idx = 1
+    while idx < N:
+        ret = (ret + v[idx]) / 2
+        idx += 1
+    print(ret)
+
+N = int(input())
+v = sorted(list(map(int, input().split())))
+solve()
