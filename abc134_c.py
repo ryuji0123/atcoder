5aa9b34deb95f11330c3d554b81bcc0367164730
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-06 22:48:38 +0900

    add abc134_c.py

--- abc134_c.py
+++ abc134_c.py
@@ -1,13 +1,11 @@
+def solve():
+    m1, m2 = sorted(A, reverse=True)[:2]
+    for a in A:
+        print(m1 if m1 != a else m2)
 
-if __name__ == '__main__':
-    N = int(input())
-    A = []
-    for i in range(N):
-        A.append(int(input()))
-    
-    B = sorted(A)
-    for n in A:
-        if B[-1] != n:
-            print(B[-1])
-        else:
-            print(B[-2])
+
+N = int(input())
+A = []
+for _ in range(N):
+    A.append(int(input()))
+solve()
