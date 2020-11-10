ad94ffc80092d9330735d057a3823cefc11cbd00
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-11 08:48:40 +0900

    modify abc120_c.py

--- abc120_c.py
+++ abc120_c.py
@@ -1,37 +1,15 @@
-class Stack(object):
-    def __init__(self):
-        self.container = []
+from collections import deque
 
-    def __len__(self):
-        return len(self.container)
 
-    def push(self, elem):
-        self.container.append(elem)
-
-    def pop(self):
-        index = len(self.container) - 1
-        if index < 0:
-            return None
-        ret = self.container[index]
-        del self.container[-1]
-        return ret
-    
-    def compare(self, val):
-        return self.container[-1] == val
-
-if __name__ == '__main__':
-    string = [int(i) for i in input()]
-    ret = 0
-    stack = Stack()
-    stack.push(string[0])
-    for s in string[1:]:
-        if stack.__len__() == 0:
-            stack.push(s)
-            continue
-        if stack.compare(s):
-            stack.push(s)
+def solve():
+    stack = deque()
+    for c in S:
+        if len(stack) == 0 or stack[-1] == c:
+            stack.append(c)
         else:
-            ret += 2
             stack.pop()
+    print(len(S) - len(stack))
 
-    print(ret)
+
+S = input()
+solve()
