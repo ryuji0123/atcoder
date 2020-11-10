0e059238b2e0372a5dc28f4aec08fc65ceca9eb3
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-11 08:54:09 +0900

    modify abc118_c.py

--- abc118_c.py
+++ abc118_c.py
@@ -1,18 +1,13 @@
-def gcd(a, b):
-    if b > a:
-        return gcd(b, a)
-    if a % b == 0:
-        return b
-    return gcd(b, a % b)
+from math import gcd
 
-if __name__ == '__main__':
-    N = int(input())
-    nums = [int(n) for n in input().split()]
-    if len(nums) == 1:
-        print(nums[0])
-        exit()
-    
-    ret = gcd(nums[0], nums[1])
-    for n in nums[1:]:
-        ret = gcd(ret, n)    
-    print(ret)
+
+def solve():
+    g = 0
+    for a in A:
+        g = gcd(g, a)
+    print(g)
+
+
+N = input()
+A = list(map(int, input().split()))
+solve()
