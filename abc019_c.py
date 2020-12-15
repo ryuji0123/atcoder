447c686678407d35e63c1093aeaaad884caa843b
ryuji0123 <ryo1011.javelin@gmail.com>
2020-12-15 23:22:06 +0900

    add ./abc019_c.py

--- abc019_c.py
+++ abc019_c.py
@@ -1,34 +1,9 @@
-def factorization(n):
-    arr = []
-    tmp = n
-    div = 2
-    while div * div <= n:
-        if tmp % div == 0:
-            cnt = 0
-            while tmp % div == 0:
-                cnt += 1
-                tmp //= div
-            arr.append([div, cnt])
-        div += 1
-
-    if tmp != 1:
-        arr.append([tmp, 1])
-
-    if len(arr) == 0:
-        arr.append([n, 1])
-
-    return arr
-
 def solve():
     ret = set()
     for a in A:
-        tmp = 1
-        divs = factorization(a)
-        for e, c in divs:
-            if e == 2:
-                continue
-            tmp *= pow(e, c)
-        ret.add(tmp)
+        while a % 2 == 0:
+            a //= 2
+        ret.add(a)
     print(len(ret))
 
 
