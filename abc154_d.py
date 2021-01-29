10099aae9200586f77232bfc31e9b55b4680497e
ryuji0123 <ryo1011.javelin@gmail.com>
2021-01-29 23:53:35 +0900

    modify abc154_d

--- abc154_d.py
+++ abc154_d.py
@@ -1,6 +1,5 @@
 def solve():
     mean_list = [(p + 1) / 2 for p in P]
-    idx = 0
     ret = cur = sum(mean_list[idx: K])
     for i in range(1, N - K + 1):
         cur += -mean_list[i - 1]  + mean_list[i + K - 1]
